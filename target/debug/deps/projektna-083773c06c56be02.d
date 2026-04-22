600f6674eaa3851e278dec964a03b7a13414781a
jmik05 <janus.miklavcic@gmial.com>
2026-04-22 11:53:03 +0200

    prvi

---
+++ target/debug/deps/projektna-083773c06c56be02.d
@@ -0,0 +1,5 @@
+C:\Users\janus\Desktop\faks\Prog 2 projekt\Projekt-pri-Programiranju-2\target\debug\deps\projektna-083773c06c56be02.d: src\main.rs
+
+C:\Users\janus\Desktop\faks\Prog 2 projekt\Projekt-pri-Programiranju-2\target\debug\deps\libprojektna-083773c06c56be02.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/projektna-0c53f631cd13ef47.d
@@ -0,0 +1,5 @@
+C:\Users\janus\Desktop\faks\Prog 2 projekt\Projekt-pri-Programiranju-2\target\debug\deps\projektna-0c53f631cd13ef47.d: src\main.rs
+
+C:\Users\janus\Desktop\faks\Prog 2 projekt\Projekt-pri-Programiranju-2\target\debug\deps\libprojektna-0c53f631cd13ef47.rmeta: src\main.rs
+
+src\main.rs:
